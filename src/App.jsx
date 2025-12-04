fe6d66eb79af58eec1060c2190265448d188ec29
“Touhami <touhami.aglagal.us@gmail.com>
2025-12-04 09:58:32 +0100

    feat: Implement application routing with React Router, replacing boilerplate UI and a simple list with dedicated routes for Pokedex pages.

--- src/App.jsx
+++ src/App.jsx
@@ -34,33 +34,20 @@ function App() {
 
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <ul>
-        {pokemons?.map((pokemon) => (
-          <li key={pokemon.id}>{pokemon.name}</li>
-        ))}
-      </ul>
-    </>
+    <Routes>
+      {/* === Pages publiques === */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+
+      {/* === Pages protégées === */}
+      {/* <Route element={<ProtectedRoute />}> */}
+      {/* <Route element={<MainLayout />}> */}
+      <Route index element={<Home />} />
+      <Route path="/Catch" element={<CatchPage />} />
+      <Route path="/Pokedex" element={<PokedexPage />} />
+      {/* </Route> */}
+      {/* </Route> */}
+    </Routes>
   )
 }
 
